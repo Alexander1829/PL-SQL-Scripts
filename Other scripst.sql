c14abf25ac13a939bcdec9f9802a9b3cc1195dbc
Alexander Maslov <maslov.am.msu@gmail.com>
2020-10-23 00:36:18 +0300

    Update Other scripst.sql

--- Other scripst.sql
+++ Other scripst.sql
@@ -1,39 +1,16 @@
-SELECT COUNT(*) 
-FROM (SELECT DISTINCT * FROM "table name"); -- Считаем количество уникальных строк в таблице
-
-select col_1, count(1) -- col_1 и количество повторений 
-from "table name"
-group by col_1
-having count(1) > 1;
-
-delete from "table name" -- удаляем все дубли из таблицы
-where rowid not in
+DELETE FROM "table_name" -- Р±С‹СЃС‚СЂРѕ РІС‹С‡РёСЃС‚РёС‚СЊ С‚Р°Р±Р»РёС†Сѓ РѕС‚ РґСѓР±Р»РёСЂСѓСЋС‰РёС…СЃСЏ СЃС‚СЂРѕРє
+WHERE rowid not in
 (
-  select ri from 
+  SELECT ri FROM 
   (
-    select min(rowid) as ri 
+    SELECT min(rowid) as ri 
           ,col_1          
           /*...*/ 
           ,col_n 
-    from  "table name"
-    group by 
+    FROM  "table_name"
+    GROUP BY 
            col_1
           /*...*/ 
           ,col_n
-  )t1); commit;
-  
-
-/*Следующий запрос тоже удаляет все дубли, но работает медленнее, в несколько раз.*/
-delete from "table name"
-where rowid in
-(
-  select rowid from
-  (
-    select rowid, row_number() over (partition by col_1,col_2,col_3 /*,...*/ 
-                                     order by rowid) rn
-    from "table name" 
-  ) t
-  where rn != 1
-); commit;
-
-
+  )t1); 
+--COMMIT;
