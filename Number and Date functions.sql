3d1fc17645ecff87ffca52765e23d8bddf5cb216
Alexander Maslov <maslov.am.msu@gmail.com>
2020-10-23 01:06:33 +0300

    +

--- Number and Date functions.sql
+++ Number and Date functions.sql
@@ -1,16 +1,14 @@
-/*Функции по работе с числами*/
-select
+SELECT
      round(3.5)     --4
     ,round(3.1415,2)--3.14
     ,round(3.1415,3)--3.142
     ,trunc(3.5)     --3 
     ,trunc(3.1415,3)--3.141
-    ,ceil(3.1415)   --4 --целая часть +1
-from dual;
+    ,ceil(3.1415)   --4 --Г¶ГҐГ«Г Гї Г·Г Г±ГІГј +1
+FROM dual;
 
 --Alter session set nls_date_format='DD-MON-YYYY'
-/*Функции по работе с датами*/
-select
+SELECT
      sysdate + 1
     ,sysdate - 1.5
     ,sysdate + 1/24/60
@@ -26,5 +24,5 @@ select
     ,to_char(sysdate, 'dd-mon-yyyy', 'NLS_DATE_LANGUAGE=''RUSSIAN''')
     ,add_months(sysdate,1) 
     ,months_between(to_date('30.11.2017', 'dd.mm.yyyy'),to_date('31.01.2017', 'dd.mm.yyyy')) 
-from dual;
+FROM dual;
 
