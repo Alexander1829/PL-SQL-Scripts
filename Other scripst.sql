3ad4df679270333fb01e63fab3aea08b196db5c1
Alexander Maslov <maslov.am.msu@gmail.com>
2020-10-23 00:56:18 +0300

    +

--- Other scripst.sql
+++ Other scripst.sql
@@ -1,4 +1,5 @@
-DELETE FROM "table_name" -- быстро вычистить таблицу от дублирующихся строк
+-- Вычистить таблицу от дублирующихся строк - оставить только уникальные
+DELETE FROM "table_name" 
 WHERE rowid not in
 (
   SELECT ri FROM 
@@ -14,3 +15,19 @@ WHERE rowid not in
           ,col_n
   )t1); 
 --COMMIT;
+
+
+--Добавление внешнего ключа
+--Добавить внешний ключ к таблице CS_PR_TRANSPORT Т1
+ALTER TABLE SCHEM.TBL ADD (
+--Название ключа
+  CONSTRAINT TBL_FK 
+--поле COLUMN_1 в TBL, может быть НЕ уникальным 
+  FOREIGN KEY (COLUMN_1) 
+--поле ID в SCHEM.TBL2 Т2 первичный ключ => уникальное   
+  REFERENCES SCHEM.TBL2 (ID)
+  ENABLE VALIDATE);
+
+-- Инфо о нужной таблице
+-- Например количество строк, чтобы не делать SELECT count(1) 
+SELECT num_rows FROM all_tables WHERE table_name = 'NSM_MONTH_TRANSACTIONS'
