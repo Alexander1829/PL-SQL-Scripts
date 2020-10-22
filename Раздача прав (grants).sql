cd13a106ef198becd3671a47e685a53c7c99a8e5
Alexander Maslov <maslov.am.msu@gmail.com>
2020-10-23 00:47:01 +0300

    +

--- Раздача прав (grants).sql
+++ Раздача прав (grants).sql
@@ -1,10 +1,17 @@
-/*Раздача прав доступа к объектам текущей схемы для других пользователей*/
+GRANT select ON scheme1.tab1 TO scheme2; 
+GRANT insert, update, delete ON scheme1.tab1 TO scheme2; 
+REVOKE insert, update, delete ON scheme1.tab1 FROM scheme2; /*Р—Р°Р±СЂР°С‚СЊ РїСЂР°РІР°*/
+GRANT alter ON scheme1.tab1 TO scheme2; 
+GRANT all ON scheme1.tab1 TO scheme2; 
+GRANT execute ON scheme1.pkg1 TO scheme2;
+GRANT create database link TO scheme2; 
 
-Grant select on scheme1.tab1 to scheme2; /*Дать права на SELECT из таблицы TAB1*/
-Grant insert, update, delete on scheme1.tab1 to scheme2; /*Дать права на insert, update, delete таблицы TAB1*/
-Revoke insert, update, delete on scheme1.tab1 from scheme2; /*Забрать права на insert, update, delete таблицы TAB1*/
-Grant alter on scheme1.tab1 to scheme2; /*Дать права на alter таблицы TAB1*/
-Grant all on scheme1.tab1 to scheme2; /*Дать все права на таблицу TAB1*/
-Grant execute on scheme1.pkg1 to scheme2; /*Дать права на выполнение PKG1*/
-Grant create database link to scheme2; /*Дать права на создание ссылки на схему*/
-
+---------------
+CREATE USER otheruser IDENTIFIED BY unknown_pwd;
+GRANT create session TO otheruser;
+CREATE USER poweruser IDENTIFIED BY known_pwd;
+GRANT create session TO poweruser;
+GRANT create database link TO poweruser WITH ADMIN OPTION;
+GRANT create any procedure TO poweruser;
+GRANT drop any procedure TO poweruser;
+GRANT execute any procedure TO poweruser;
