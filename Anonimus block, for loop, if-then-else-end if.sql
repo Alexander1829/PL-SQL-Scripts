4c4e84522a4edfd7c155b35a900c6523a60e9641
Alexander Maslov <maslov.am.msu@gmail.com>
2020-11-05 00:57:53 +0300

    +

--- Anonimus block, for loop, if-then-else-end if.sql
+++ Anonimus block, for loop, if-then-else-end if.sql
@@ -1,13 +1,12 @@
+/*
 create table temp_test_clients (id integer);
-
-insert into  temp_test_clients (id) values (0);
-insert into  temp_test_clients (id) values (1);
-insert into  temp_test_clients (id) values (2);
-insert into  temp_test_clients (id) values (3);
-insert into  temp_test_clients (id) values (4);
-
 create table temp_test_clients_1 as select * from temp_test_clients where 1=0;
 create table temp_test_clients_2 as select * from temp_test_clients where 1=0;
+
+insert into  temp_test_clients (id) values (0); insert into  temp_test_clients (id) values (1);
+insert into  temp_test_clients (id) values (2); insert into  temp_test_clients (id) values (3);
+insert into  temp_test_clients (id) values (4);
+*/
 
 begin
   for z in (select id from temp_test_clients) loop
